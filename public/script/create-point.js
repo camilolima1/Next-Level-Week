1b6c3e60531671ecab13dcbdbb332d760b0bf27a
camilolima <caamilolima@gmail.com>
2020-07-06 10:28:21 -0300

    Aplicação Ecoleta Web

---
+++ public/script/create-point.js
@@ -0,0 +1,100 @@
+
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("http://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=name")
+        .then( res => res.json() )
+        .then( states => {
+            for(const state of states){
+                ufSelect.innerHTML 
+                    += `<option value="${state.id}">${state.nome}</option>`
+            }
+    } )
+}
+
+populateUFs()
+
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+        .then( res => res.json() )
+        .then( cities => {
+            
+            for(city of cities){
+                citySelect.innerHTML 
+                    += `<option value="${city.nome}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false
+    } )
+
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+
+//Ítens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe com java script
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // console.log('ITEM ID', itemId)
+
+    //ver se tem itens selecionados
+    //se sim, pegar eles
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId  //true ou false
+        return itemFound 
+    })
+
+
+    //se já tiver selecionado, tem que tirar da seleção
+    if( alreadySelected >= 0 ){
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        //se não tiver tem que adicionar a seleção
+        selectedItems.push(itemId)
+    }
+    // console.log('selectedItems', selectedItems)
+
+    //atualizar o campo, input, escondido
+    collectedItems.value = selectedItems
+    
+}
